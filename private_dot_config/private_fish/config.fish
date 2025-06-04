6ed84bae965c195ad056734f7f55fcf263922f4d
Rasmus Ravn Andersen <rasmus-ravn-andersen@protonmail.com>
2025-06-04 12:29:59 +0200

    Add .config/fish/config.fish Add .config/sway/config

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,16 @@
+alias cat bat
+alias rm "rm -i"
+alias cp "cp -i"
+alias vim nvim
+alias mv "mv -i"
+alias mkdir "mkdir -p"
+alias h history
+alias which "type -a"
+set -x FZF_DEFAULT_COMMAND 'ag -g "" --hidden --ignore .git'
+set -Ux EDITOR nvim
+set -Ux GITHUB_USERNAME "rasmusravn"
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+starship init fish | source
+zoxide init fish | source
