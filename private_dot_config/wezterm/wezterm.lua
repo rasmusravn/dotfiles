43e645262aceee8cf030f82488e8510cef9a5482
Rasmus Ravn Andersen <rasmus-ravn-andersen@protonmail.com>
2025-06-04 18:42:55 +0200

    Add .config/wezterm/wezterm.lua

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require("wezterm")
+
+return {
+	font = wezterm.font_with_fallback({
+		--"JetBrains Mono",
+		"Noto Color Emoji",
+	}),
+	window_padding = {
+		left = 20,
+		right = 10,
+		top = 10,
+		bottom = 10,
+	},
+	window_background_opacity = 0.96,
+	color_scheme = "Gruvbox Dark (Gogh)",
+	enable_tab_bar = false,
+}
