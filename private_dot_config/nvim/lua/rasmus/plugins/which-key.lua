58179b65b42b5fe95501dc61da6f8b2ae42f78c6
Rasmus Ravn Andersen <rasmus-ravn-andersen@protonmail.com>
2025-06-17 17:42:26 +0200

    Update .config/nvim/lua/rasmus/plugins/which-key.lua

--- private_dot_config/nvim/lua/rasmus/plugins/which-key.lua
+++ private_dot_config/nvim/lua/rasmus/plugins/which-key.lua
@@ -1,24 +1,37 @@
 return {
-  'folke/which-key.nvim',
-  event = 'VimEnter',
-  config = function()
-    local wk = require 'which-key'
+	"folke/which-key.nvim",
+	event = "VimEnter",
+	config = function()
+		-- In your which-key setup file:
+		local wk = require("which-key")
 
-    wk.setup()
+		wk.register({
+			-- Normal mode <leader> mappings (old "+Name" becomes group = "Name")
+			{ "<leader>c", group = "Code" },
+			{ "<leader>d", group = "Document" },
+			{ "<leader>h", group = "Git Hunk" }, -- This would be the group for git hunk related commands
+			{ "<leader>r", group = "Rename" },
+			{ "<leader>s", group = "Search" },
+			{ "<leader>t", group = "Toggle" },
+			{ "<leader>w", group = "Workspace" },
 
-    wk.register({
-      c = { name = '+Code' },
-      d = { name = '+Document' },
-      r = { name = '+Rename' },
-      s = { name = '+Search' },
-      w = { name = '+Workspace' },
-      t = { name = '+Toggle' },
-      h = { name = 'Git Hunk' },
-    }, { prefix = '<leader>' })
+			-- Your actual Git Hunk command for normal mode (if you have one at <leader>h)
+			-- If `<leader>h` *is* the command, you'd change the above line to:
+			-- { "<leader>h", desc = "Git Hunk (command name)", mode = "n" },
+			-- and make any actual sub-commands like `<leader>h s` for `Git Hunk Stage` etc.
 
-    -- visual mode
-    wk.register({
-      h = { 'Git Hunk' },
-    }, { prefix = '<leader>', mode = 'v' })
-  end,
+			-- Visual mode <leader>h mapping
+			{ "<leader>h", desc = "Git Hunk (Visual Mode)", mode = "v" },
+			-- Note: The `mode = "v"` here explicitly states it's for visual mode.
+			-- You might have specific commands nested under `<leader>h` in visual mode too,
+			-- so this `desc` should reflect what that specific `<leader>h` *does* in visual mode.
+		}, { prefix = "<leader>" })
+
+		-- Important: Any nested mappings for +Code, +Document, etc.,
+		-- should continue to be registered within their respective groups.
+		-- For example:
+		-- wk.register({
+		--   f = { ":Telescope find_files<CR>", "Find Files" },
+		-- }, { prefix = "<leader>c" }) -- This means <leader>c f
+	end,
 }
