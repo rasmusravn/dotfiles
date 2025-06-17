bf445f15e383a0f753055c63781e5bfebebc274e
Rasmus Ravn Andersen <rasmus-ravn-andersen@protonmail.com>
2025-06-17 17:43:45 +0200

    Update .config/nvim/lua/rasmus/plugins/which-key.lua

--- private_dot_config/nvim/lua/rasmus/plugins/which-key.lua
+++ private_dot_config/nvim/lua/rasmus/plugins/which-key.lua
@@ -6,26 +6,15 @@ return {
 		local wk = require("which-key")
 
 		wk.register({
-			-- Normal mode <leader> mappings (old "+Name" becomes group = "Name")
-			{ "<leader>c", group = "Code" },
-			{ "<leader>d", group = "Document" },
-			{ "<leader>h", group = "Git Hunk" }, -- This would be the group for git hunk related commands
-			{ "<leader>r", group = "Rename" },
-			{ "<leader>s", group = "Search" },
-			{ "<leader>t", group = "Toggle" },
-			{ "<leader>w", group = "Workspace" },
-
-			-- Your actual Git Hunk command for normal mode (if you have one at <leader>h)
-			-- If `<leader>h` *is* the command, you'd change the above line to:
-			-- { "<leader>h", desc = "Git Hunk (command name)", mode = "n" },
-			-- and make any actual sub-commands like `<leader>h s` for `Git Hunk Stage` etc.
-
-			-- Visual mode <leader>h mapping
-			{ "<leader>h", desc = "Git Hunk (Visual Mode)", mode = "v" },
-			-- Note: The `mode = "v"` here explicitly states it's for visual mode.
-			-- You might have specific commands nested under `<leader>h` in visual mode too,
-			-- so this `desc` should reflect what that specific `<leader>h` *does* in visual mode.
-		}, { prefix = "<leader>" })
+			{ "<leader>", group = "Workspace" },
+			{ "<leader>", group = "Git Hunk" },
+			{ "<leader>", group = "Rename" },
+			{ "<leader>", group = "Search" },
+			{ "<leader>", group = "Document" },
+			{ "<leader>", group = "Code" },
+			{ "<leader>", group = "Toggle" },
+			{ "<leader>", desc = "<leader>h", mode = "v" },
+		})
 
 		-- Important: Any nested mappings for +Code, +Document, etc.,
 		-- should continue to be registered within their respective groups.
