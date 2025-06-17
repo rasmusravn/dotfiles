ac530b62f05275b434057890c60c2575d39b3831
Rasmus Ravn Andersen <rasmus-ravn-andersen@protonmail.com>
2025-06-17 17:12:23 +0200

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -82,7 +82,8 @@ require("lazy").setup({
 	require("rasmus.plugins.vimtex"),
 	require("rasmus.plugins.obsidian"),
 	require("rasmus.plugins.oil"),
-	require("rasmus.plugins.copilot")
+	require("rasmus.plugins.copilot"),
+	require("rasmus.plugins.mason-nvim-dap"),
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
