5d0fdb6cd69719d7b825b7cb2c403a7839fefd10
Rasmus Ravn Andersen <rasmus-ravn-andersen@protonmail.com>
2025-06-17 13:37:54 +0200

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -7,6 +7,8 @@ alias h history
 alias which "type -a"
 alias cm chezmoi
 alias ce "chezmoi edit --apply"
+alias mount_pi "sudo sshfs -o allow_other,default_permissions
+pi@192.168.0.10:/home/pi/ /mnt/pi"
 set -x FZF_DEFAULT_COMMAND 'ag -g "" --hidden --ignore .git'
 set -Ux EDITOR nvim
 set -Ux GITHUB_USERNAME "rasmusravn"
