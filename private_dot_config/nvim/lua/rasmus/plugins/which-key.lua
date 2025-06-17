dcd623643ada6b32915fed2eb4e94730996a1260
Rasmus Ravn Andersen <rasmus-ravn-andersen@protonmail.com>
2025-06-17 17:52:06 +0200

    Update .config/nvim/lua/rasmus/plugins/which-key.lua

--- private_dot_config/nvim/lua/rasmus/plugins/which-key.lua
+++ private_dot_config/nvim/lua/rasmus/plugins/which-key.lua
@@ -1,26 +1,58 @@
 return {
-	"folke/which-key.nvim",
-	event = "VimEnter",
-	config = function()
-		-- In your which-key setup file:
-		local wk = require("which-key")
+	{ -- Useful plugin to show you pending keybinds.
+		"folke/which-key.nvim",
+		event = "VimEnter", -- Sets the loading event to 'VimEnter'
+		opts = {
+			-- delay between pressing a key and opening which-key (milliseconds)
+			-- this setting is independent of vim.o.timeoutlen
+			delay = 0,
+			icons = {
+				-- set icon mappings to true if you have a Nerd Font
+				mappings = vim.g.have_nerd_font,
+				-- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+				-- default which-key.nvim defined Nerd Font icons, otherwise define a string table
+				keys = vim.g.have_nerd_font and {} or {
+					Up = "<Up> ",
+					Down = "<Down> ",
+					Left = "<Left> ",
+					Right = "<Right> ",
+					C = "<C-…> ",
+					M = "<M-…> ",
+					D = "<D-…> ",
+					S = "<S-…> ",
+					CR = "<CR> ",
+					Esc = "<Esc> ",
+					ScrollWheelDown = "<ScrollWheelDown> ",
+					ScrollWheelUp = "<ScrollWheelUp> ",
+					NL = "<NL> ",
+					BS = "<BS> ",
+					Space = "<Space> ",
+					Tab = "<Tab> ",
+					F1 = "<F1>",
+					F2 = "<F2>",
+					F3 = "<F3>",
+					F4 = "<F4>",
+					F5 = "<F5>",
+					F6 = "<F6>",
+					F7 = "<F7>",
+					F8 = "<F8>",
+					F9 = "<F9>",
+					F10 = "<F10>",
+					F11 = "<F11>",
+					F12 = "<F12>",
+				},
+			},
 
-		wk.register({
-			{ "<leader>", group = "Workspace" },
-			{ "<leader>", group = "Git Hunk" },
-			{ "<leader>", group = "Rename" },
-			{ "<leader>", group = "Search" },
-			{ "<leader>", group = "Document" },
-			{ "<leader>", group = "Code" },
-			{ "<leader>", group = "Toggle" },
-			{ "<leader>", desc = "<leader>h", mode = "v" },
-		})
-
-		-- Important: Any nested mappings for +Code, +Document, etc.,
-		-- should continue to be registered within their respective groups.
-		-- For example:
-		-- wk.register({
-		--   f = { ":Telescope find_files<CR>", "Find Files" },
-		-- }, { prefix = "<leader>c" }) -- This means <leader>c f
-	end,
+			-- Document existing key chains
+			spec = {
+				{ "<leader>s", group = "Search" },
+				{ "<leader>w", group = "Workspace" },
+				{ "<leader>h", group = "Git Hunk" },
+				{ "<leader>r", group = "Rename" },
+				{ "<leader>d", group = "Document" },
+				{ "<leader>c", group = "Code" },
+				{ "<leader>t", group = "Toggle" },
+			},
+		},
+	},
 }
