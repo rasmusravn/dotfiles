ff054aa7cb5ddc3691aea5824a43707f15094bce
Rasmus Ravn <YOUR_EMAIL@example.com>
2025-07-05 10:38:03 +0200

    Update .config/fish/conf.d/30_functions.fish

--- dot_config/fish/conf.d/30_functions.fish
+++ dot_config/fish/conf.d/30_functions.fish
@@ -5,5 +5,8 @@ end
 function work
 	swaymsg workspace 2; swaymsg exec firefox
 	swaymsg workspace 3; swaymsg exec obsidian
+	sleep 1
 	swaymsg workspace 4; swaymsg exec proton-mail
+	sleep 1
+	swaymsg workspace 1
 end
