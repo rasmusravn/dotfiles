564120b534002cd2c343a07f183c6e32e290a553
Rasmus Andersen <rasmus.andersen@cobhamsatcom.com>
2025-11-13 09:04:24 +0000

    Update .config/fish/conf.d/20_env.fish

--- dot_config/fish/conf.d/20_env.fish
+++ dot_config/fish/conf.d/20_env.fish
@@ -4,5 +4,4 @@ set -Ux EDITOR nvim
 set -Ux GITHUB_USERNAME "rasmusravn"
 set -Ux GITHUB_EMAIL "git.pavilion936@passmail.com"
 starship init fish | source
-zoxide init fish | source
 #fnm env --use-on-cd | source
