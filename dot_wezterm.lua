0db976ad094999f737cdc91b8b1cfa63b37b1995
Rasmus Ravn Andersen <rasmus-ravn-andersen@protonmail.com>
2026-02-17 10:47:53 +0100

    stylua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,16 +1,16 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font_with_fallback({
-		"JetBrains Mono",
-	}),
-	window_padding = {
-		left = 20,
-		right = 10,
-		top = 10,
-		bottom = 10,
-	},
-	window_background_opacity = 0.76,
-	color_scheme = "Gruvbox Dark (Gogh)",
-	enable_tab_bar = false,
+  font = wezterm.font_with_fallback({
+    "JetBrains Mono",
+  }),
+  window_padding = {
+    left = 20,
+    right = 10,
+    top = 10,
+    bottom = 10,
+  },
+  window_background_opacity = 0.76,
+  color_scheme = "Gruvbox Dark (Gogh)",
+  enable_tab_bar = false,
 }
