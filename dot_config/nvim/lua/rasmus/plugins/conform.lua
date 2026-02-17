2d0d0509df833dcc8d1b5f3388e4211190b1f3e2
Rasmus Ravn Andersen <rasmus-ravn-andersen@protonmail.com>
2026-02-17 10:10:43 +0100

    Update .config/nvim/lua/rasmus/plugins/conform.lua

--- dot_config/nvim/lua/rasmus/plugins/conform.lua
+++ dot_config/nvim/lua/rasmus/plugins/conform.lua
@@ -1,15 +1,15 @@
 return {
-  'stevearc/conform.nvim',
-  event = { 'BufWritePre' },
-  cmd = { 'ConformInfo' },
-  opts = {
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      python = { 'ruff_format', 'black' },
-    },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-  },
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "ruff_format", "black" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
 }
